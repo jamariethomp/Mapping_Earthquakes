c94343e8b3941cbecb78527ebf8e810ecbadf59d
Jamie Thompson <jamariethomp@gmail.com>
2022-03-19 16:20:06 -0600

    Update file. jmt

--- Mapping_GeoJSON_Data/static/js/logic.js
+++ Mapping_GeoJSON_Data/static/js/logic.js
@@ -12,7 +12,7 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 streets.addTo(map);
 
 // Accessing the airport GeoJSON URL
-let airportData = "https://raw.githubusercontent.com/<GitHub_name>/Mapping_Earthquakes/main/majorAirports.json";
+let airportData = "https://raw.githubusercontent.com/jamariethomp/Mapping_Earthquakes/main/majorAirports.json";
 
 // Grabbing our GeoJSON data.
 d3.json(airportData).then(function(data) {
